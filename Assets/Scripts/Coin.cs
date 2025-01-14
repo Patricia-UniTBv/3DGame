cd8c7af41d6220d4379924be6118070c1258d0b0
Donisan Patricia-Anelis <115454818+Patricia-UniTBv@users.noreply.github.com>
2025-01-14 18:31:27 +0100

    Coin Collecter implementation
    Initial commit

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Coin : MonoBehaviour
+{
+    public float rotateSpeed;
+    public AudioClip coinSound;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = Camera.main.GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            Debug.LogError("Audio Source not found!");
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(0, rotateSpeed * Time.deltaTime, 0, Space.Self);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        audioSource.PlayOneShot(coinSound);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,115 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    float xInput;
+    float yInput;
+
+    Rigidbody rb;
+
+    int coinsCollected;
+    public int totalCoins; 
+
+    public AudioClip coinSound;
+    private AudioSource audioSource;
+
+    public float gameDuration = 60f; 
+    private float timeRemaining;
+
+    public TMP_Text timerText; 
+    public TMP_Text endGameText; 
+
+    private bool gameEnded = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        timeRemaining = gameDuration;
+
+        endGameText.text = ""; 
+    }
+
+    private void Update()
+    {
+        if (gameEnded) return;
+
+        timeRemaining -= Time.deltaTime;
+        if (timeRemaining <= 0)
+        {
+            timeRemaining = 0;
+            EndGame(); 
+        }
+
+        UpdateTimerUI();
+
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
+        }
+
+        if (transform.position.y <= -5f)
+        {
+            SceneManager.LoadScene(0); 
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!gameEnded)
+        {
+            rb.AddForce(xInput * moveSpeed, 0, yInput * moveSpeed);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Coin")
+        {
+            audioSource.PlayOneShot(coinSound);
+            coinsCollected++;
+            other.gameObject.SetActive(false);
+
+            if (coinsCollected >= totalCoins)
+            {
+                EndGame();
+            }
+        }
+    }
+
+    private void EndGame()
+    {
+        gameEnded = true;
+        rb.linearVelocity = Vector3.zero; 
+
+        if (coinsCollected >= totalCoins)
+        {
+            endGameText.text = "You Won!";
+        }
+        else
+        {
+            endGameText.text = "Game Over!";
+        }
+
+        Invoke("RestartGame", 5f);
+    }
+
+    private void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void UpdateTimerUI()
+    {
+        timerText.text = "Time: " + Mathf.Ceil(timeRemaining).ToString() + "s";
+    }
+}
